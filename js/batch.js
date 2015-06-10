54b6791395e411deca81f6f6c7ccfb06a1635c8a
Igor V Belousov <igor@belousovv.ru>
2015-06-10 17:13:55 +0300

    fix percent

--- js/batch.js
+++ js/batch.js
@@ -1,26 +1,30 @@
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function($) {
   var batch_progress = function ( data ) {
     $( '.batch-progress-message' ).html( data['progress_message'] );
-    $( '.batch-percent' ).html( data['percent'] + "%" );
-    $( '.batch-progress > span' ).animate({ width: data['percent'] + "%" }, 500);
+    var percent = 0;
+    if ( 'undefined' != typeof data['percent'] ) {
+      percent = data['percent'];
+    }
+    $( '.batch-percent' ).html( percent + "%" );
+    $( '.batch-progress > span' ).animate({ width: percent + "%" }, 500);
   };
 
   function batch_process() {
     $.post(
-      ajaxurl+'?action=batch_operations&id='+batch_id,
-      function(data){
-        if (data['do']=='finish')
+      ajaxurl + '?action=batch_operations&id=' + batch_id,
+      function( data ) {
+        if ( 'finish' == data['do'] )
         {
           batch_progress( data );
-          $('.batch-message').html(data['message']).delay(1500).queue(function () {
-              $(location).attr('href',successful_page);
-              $(this).dequeue();
+          $( '.batch-message' ).html( data['message'] ).delay( 1500 ).queue( function () {
+              $( location ).attr( 'href', successful_page );
+              $( this ).dequeue();
             }
           );
         }
         else
         {
-          $('.batch-message').html(data['message']);
+          $( '.batch-message' ).html( data['message'] );
           batch_progress( data );
           batch_process()
         }
